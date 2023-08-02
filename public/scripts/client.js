0a2c1ac60226705a28c1769f2dcbdbdf264f696d
jvarghe <vcs.inbox@outlook.com>
2023-08-02 17:56:48 +0000

    Add event handler for '#New-Tweet-Form' to 'public/scripts/client.js'.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,7 +1,9 @@
 /*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
+ * This file the corresponding JavaScript file for `index.html`.
+ *
+ * Client-side JS logic goes here.
+ * jQuery is already loaded.
+ * Reminder: Use (and do all your DOM work in) jQuery's document ready function.
  */
 
 
@@ -11,6 +13,78 @@ $(document).ready(function() {
   // Log a message to the console once the web page has been loaded.
   // console.log("The document is ready!");
 
+
+  // HANDLING NEW TWEETS
+
+  // Event Handler: If a `Submit` event is triggered on the `#New-Tweet-Form`
+  // element (which is NOT a form, but the parent of one), it will trigger
+  // this function. It is an event handler listening for the `submit` action
+  // on the form in `#New-Tweet-Form`.
+  $("#New-Tweet-Form").on("submit", function(event) {
+
+    // The default behaviour when a form is submitted is to send the data to
+    // the backend and load the response page. This is not what we want here,
+    // so we will prevent the default response.
+    event.preventDefault();
+
+    // console.log("New Tweet Submitted!");
+
+
+    /* SERIALIZING FORM DATA (INTO A QUERY STRING OR JSON STRING)
+     *
+     * When the form was submitted, it should have sent all the data entered
+     * into it to the backend via a POST request. Specifically, this event
+     * handler should have gotten it.
+     *
+     * This form data is in raw form [Question: What format is it in?].
+     * Servers are usually configured to accept data either as JSON strings
+     * or Query Strings. This project's backend is configured to accept data
+     * in query string format. So before we can pass form data to the server's
+     * endpoints, we will have to convert it to a Query String.
+     *
+     * The exact structure of Query Strings is NOT standardized, but a common
+     * format is a set of field-value pairs, where the field is separated from
+     * the value by `=` and each pair is separated from the next pair by `&`.
+     * Example: `field1=value1&field2=value2&field3=value3...`. This is very
+     * similar to the way data is passed to the backend via the URL .
+     *
+     * You have to use jQuery's `.serialize()` function to turn a set of form
+     * data into a query string. This serialized data should be sent to the
+     * server in the `data` field of the AJAX POST request.
+     */
+
+    // console.log("Your Raw Tweet: ", $("#New-Tweet-Form form"));
+
+    const newTweet = $("#New-Tweet-Form form").serialize();
+    // console.log("Your Serialized Tweet: ", newTweet);
+
+
+    // AJAX Call: Make an AJAX call to return data from the form to the server
+    // without refreshing the web page. Note that the AJAX call takes an object
+    // (??) which contains several keys like `url`, `type`, `data`, `success`
+    // and `error`. Pass the newly created tweet to the `/tweets` on the
+    // backend.
+    $.ajax({
+
+      url: "/tweets",
+      type: "POST",
+      data: newTweet,
+
+      success: function(result) {
+        console.log("Your Tweet was Successfully Submitted!");
+      },
+
+      error: function(error) {
+        console.log("Tweet Submission Failed! Error Message: ", error);
+      }
+
+    });
+
+  });
+
+
+
+  // LOADING EXISTING TWEETS
 
   // Tweet data hardcoded here for temporary use. It will be replaced with
   // an AJAX call later.
@@ -114,7 +188,6 @@ $(document).ready(function() {
       // an HTML template body for every tweet. Append each tweet's template
       // to the Tweets Container.
       $("#Tweets-Container").append(createTweetElement(tweet));
-      console.log("Tweets Container Loop!");
     }
 
   };
